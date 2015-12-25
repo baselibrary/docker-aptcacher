bba043667dcd096e042406f504ead8650f8ac3fb
Shawn Ma <qsma@thoughtworks.com>
2015-12-25 09:16:54 +0800

    update

--- update.sh
+++ update.sh
@@ -11,12 +11,12 @@ versions=( "${versions[@]%/}" )
 
 
 for version in "${versions[@]}"; do 
-  fullVersion="$(curl -fsSL "http://archive.ubuntu.com/ubuntu/dists/trusty/universe/binary-amd64/Packages.gz" | gunzip | awk -F ': ' '$1 == "Package" { pkg = $2 } pkg ~ /^apt-cacher-ng$/ && $1 == "Version" { print $2 }' | grep "$version" | sort -rV | head -n1 )"
+  fullVersion="$(curl -fsSL "http://archive.ubuntu.com/ubuntu/dists/trusty/universe/binary-amd64/Packages.gz" | gunzip | awk -F ': ' '$1 == "Package" { pkg = $2 } pkg ~ /^apt-cacher$/ && $1 == "Version" { print $2 }' | grep "$version" | sort -rV | head -n1 )"
   (
     set -x
     sed '
-      s/%%APTCACHER_MAJOR%%/'"$version"'/g;
-      s/%%APTCACHER_VERSION%%/'"$fullVersion"'/g;
+      s/%%APTCACHE_MAJOR%%/'"$version"'/g;
+      s/%%APTCACHE_VERSION%%/'"$fullVersion"'/g;
     ' Dockerfile.template > "$version/Dockerfile"
   )
 done
